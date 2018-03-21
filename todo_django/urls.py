371324ad860adb924b424efcfb497f2fd4bd64e0
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-03-20 19:50:47 -0500

    Update urlconf for Django 2.0

--- todo_django/urls.py
+++ todo_django/urls.py
@@ -1,29 +1,14 @@
-"""tasks_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 from todo import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.TodoListView.as_view(), name='todo-list'),
-    url(r'^add/$', views.TodoAddView.as_view(), name='todo-add'),
-    url(
-        r'^done/(?P<todo_id>\d+)/$',
+    path('admin/', admin.site.urls),
+    path('', views.TodoListView.as_view(), name='todo-list'),
+    path('add/', views.TodoAddView.as_view(), name='todo-add'),
+    path(
+        'done/<int:todo_id>/',
         views.TodoDoneView.as_view(),
         name='todo-done'
     )
